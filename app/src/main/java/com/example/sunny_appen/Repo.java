a35324f73a098d639526cd90f2faa6b8388705e5
Andre <36155981+Neaxic@users.noreply.github.com>
2022-05-19 01:19:02 +0200

    recycler

--- app/src/main/java/com/example/sunny_appen/Repo.java
+++ app/src/main/java/com/example/sunny_appen/Repo.java
@@ -1,4 +1,39 @@
 package com.example.sunny_appen;
 
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.ArrayList;
+
 public class Repo {
+
+    //Center for data
+    private static Repo instance;
+    private MutableLiveData<ArrayList<Player>> players;
+    private MutableLiveData<ArrayList<Point>> points;
+
+    private Repo(){
+        players = new MutableLiveData<>();
+        points = new MutableLiveData<>();
+
+        Player d = new Player();
+        d.setNavn("Bente");
+
+        ArrayList<Player> p = new ArrayList<>();
+        p.add(d);
+
+        players.postValue(p);
+    }
+
+    public static synchronized Repo getInstance(){
+        if(instance == null){
+            instance = new Repo();
+        }
+        return instance;
+    }
+
+    public MutableLiveData<ArrayList<Player>> getPlayers() {
+        return players;
+    }
+
+
 }
--- app/src/main/java/com/example/sunny_appen/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/sunny_appen/ui/home/HomeFragment.java
@@ -9,12 +9,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.sunny_appen.R;
+import com.example.sunny_appen.Repo;
 import com.example.sunny_appen.databinding.FragmentHomeBinding;
+import com.example.sunny_appen.recycler.OnClickListener;
+import com.example.sunny_appen.recycler.RecAdapter;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private Repo repo;
+    private RecyclerView playerList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,11 +31,33 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        repo = Repo.getInstance();
+
+
+        setText();
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
+
+    private void setText(){
+        playerList = binding.getRoot().findViewById(R.id.rv);
+        playerList.hasFixedSize();
+        playerList.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        repo.getPlayers().observe(getViewLifecycleOwner(), player -> {
+            RecAdapter b = new RecAdapter(player);
+            playerList.setAdapter(b);
+
+            b.setOnClickListner(p -> {
+                System.out.println(p.getNavn());
+            });
+        });
+
+    }
+
+
 
     @Override
     public void onDestroyView() {
